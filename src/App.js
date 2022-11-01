6138cc0b77d8d1ba0396c683aebaf55d5f380fa3
Osnielky <osnielkisrq@gmail.com>
2022-11-01 10:43:38 -0400

    max profit added

--- src/App.js
+++ src/App.js
@@ -5,29 +5,23 @@ import "./App.css";
 // represented by seven different symbols: I, V, X, L, C, D and M.)
 
 var romanToInt = function (s) {
-  let total = trans(s.charAt(s.length-1));
+  let total = trans(s.charAt(s.length - 1));
   let last = 0;
   let past = 1001;
   let currentNumber = 0;
 
   for (let index = s.length - 2; index >= 0; index--) {
     let actual = s.charAt(index);
-    past = s.charAt(index +1 );
+    past = s.charAt(index + 1);
     if (trans(actual) >= trans(past)) {
       total += trans(actual);
     } else {
-  total-=trans (actual)
-
-
-
+      total -= trans(actual);
     }
   }
 
   return total;
 };
-
-
-
 
 function trans(params) {
   switch (params) {
@@ -54,11 +48,30 @@ function trans(params) {
   }
 }
 
+//121. Best Time to Buy and Sell Stock
+//brute force
+
+var maxProfit = function (prices) {
+  let profit = 0;
+
+  let min = Number.MAX_SAFE_INTEGER + 1;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
+    } else if (prices[i] - min > profit) {
+      profit = prices[i] - min;
+    }
+  }
+
+  return profit;
+};
+
 function App() {
   return (
     <div className="App">
       <h1>Template is running</h1>
-      <h1>{romanToInt("MCMXCIV")}</h1>
+      <h1>{maxProfit([7, 1, 5, 3, 6, 4])}</h1>
     </div>
   );
 }
